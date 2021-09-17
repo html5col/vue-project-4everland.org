2a46adf419733e639f32ff061e6a8a6e6c067d58
html5col <html5master@sina.com>
2021-09-17 22:19:30 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div><p>Accelerated & hosted by 4EVERLAND<p></div>
   </div>
 </template>
 
